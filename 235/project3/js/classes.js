f0d0dcfec10d42fa583ce77f1b620b5d5c9b1e76
Ry254 <169156005+Ry254@users.noreply.github.com>
2025-12-01 21:40:12 -0500

    basic scene item interaction working

--- 235/project3/js/classes.js
+++ 235/project3/js/classes.js
@@ -1,84 +1,107 @@
 'use strict';
 // scene
-class scene{
-    constructor(name){
+class scene {
+    constructor(name) {
         this.name = name;
         this.sceneItems = [];
     }
-    addSceneItem(name, posX, posY, descriptions, actions){
-        this.sceneItems.push(new sceneItem(
-            name, posX, posY, descriptions, actions
-        ));
+    addSceneItem(sceneItem) {
+        this.sceneItems.push(sceneItem);
     }
-    removeSceneItem(name){
-        this.sceneItems = sceneItem.filter(item => item.name != name);
+    removeSceneItem(name) {
+        this.sceneItems = sceneItems.filter(item => item.name != name);
     }
-    display(){
-        // display scene items and background
+    display() {
+        // reset scene box
+        document.querySelector("#scene").replaceChildren([]); 
+        // display background
+        // display scene items
+        for (let item of this.sceneItems) {
+            item.display();
+        }
     }
 }
 
 // item
-class item{
-    constructor(name, descriptions, actions){
+class item {
+    constructor(name, descriptions, actions) {
         this.name = name;
         this.descriptions = descriptions;
         this.actions = actions;
     }
-    display(){
-        // add to items list
-        // add onclick
-        // add onmouse
-    }
-    onMouseEnter(){
+    display = () => { }
+    onMouseEnter = (e) => {
         // change sprite
     }
-    onMouseExit(){
+    onMouseExit = (e) => {
         // change sprite
     }
-    onClick(){
+    onClick = (e) => {
+        // reset textbox
+        document.querySelector("#textbox>p").innerHTML = "";
+        document.querySelector("#textbox>ul").replaceChildren([]);
         // show random description
+        if (this.descriptions.length > 0) {
+            let randomIndex = Math.floor(Math.random() * this.descriptions.length);
+            document.querySelector("#textbox>p").innerHTML = this.descriptions[randomIndex];
+        }
         // display actions
-        for(let action of actions){
+        for (let action of this.actions) {
             action.display();
         }
     }
 }
 
 // scene item
-class sceneItem extends interactable{
-    constructor(name, posX, posY, descriptions, actions){
+class sceneItem extends item {
+    constructor(name, posX, posY, descriptions, actions, interactable = true) {
         super(name, descriptions, actions)
         this.posX = posX;
         this.posY = posY;
-        this.interactable = true;
+        this.interactable = interactable;
     }
-    constructor(name, posX, posY){
-        super(name, undefined, undefined)
-        this.posX = posX;
-        this.posY = posY;
-        this.interactable = false;
-    }
-    display(){
+    display = () => {
         // add to scene at position
+        let p = document.createElement("p");
+        p.innerHTML = this.name;
+        document.querySelector("#scene").appendChild(p);
         // if interactable
+        if (this.interactable) {
             // add onclick
+            p.onclick = this.onClick;
             // add onmouse
+        }
+    }
+}
+
+// inventory item
+class inventoryItem extends item {
+    constructor(name, descriptions, actions) {
+        super(name, descriptions, actions)
+    }
+    display = () => {
+        // add to items list
+        // add onclick
+        // add onmouse
     }
 }
 
 // actions
-class itemAction{
-    constructor(name, action, immediate = false){
+class itemAction {
+    constructor(name, action, immediate = false) {
         this.name = name;
         this.action = action;
         this.immediate = immediate;
     }
-    display(){
-        if(this.immediate){
+    display = () => {
+        if (this.immediate) {
             this.action();
             return;
         }
         // make button with action name and action as the onclick event
+        let button = document.createElement("button");
+        button.innerHTML = this.name;
+        button.onclick = this.action;
+        document.querySelector("#textbox>ul").appendChild(document.createElement("li").innerHTML = button);
     }
 }
--- 235/project3/js/main.js
+++ 235/project3/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-let sn = new scene("qwerty");
+let voidScene = new scene("void");
+voidScene.addSceneItem(new sceneItem(
+    "test item",
+    0, 0,
+    ["test description", "other i guess"],
+    [
+        new itemAction("immediate action", () => console.log("immediate action"), true),
+        new itemAction("button action", () => console.log("button action")),
+    ])
+);
+
+
+window.onload = (e) => {
+    voidScene.display();
+}
